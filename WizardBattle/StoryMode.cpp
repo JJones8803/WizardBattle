e8fe1e2bce5e852099d7a853aef2050cb96e352b
John Jones <johnjones8803@gmail.com>
2026-09-18 09:17:22 -0400

    Initializing StoryMode files to repo; To begin development ASAP

---
+++ WizardBattle/StoryMode.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "Functions.h"
+#include "StoryMode.h"
+
--- WizardBattle/StoryMode.h
+++ WizardBattle/StoryMode.h
@@ -6,3 +6,18 @@
 
 #include "Classes.h"
 #include "Functions.h"
+
+struct DialogueLine {
+    std::string speaker;
+    std::string text;
+};
+
+struct Encounter {
+    std::string chapterTitle;
+    std::vector<DialogueLine> introDialogue;
+    std::function<Wizard(const Wizard& player)> generateEnemy;
+    std::vector<DialogueLine> victoryDialogue;
+    std::function<void(Wizard& player)> onVictoryReward;
+};
+
+void runStoryMode(Wizard& player);
